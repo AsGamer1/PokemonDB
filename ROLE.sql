889a4561b1b0a36b7e35e144d9a8435627813075
ToniMatasPerello <147175095+ToniMatasPerello@users.noreply.github.com>
2024-05-28 21:13:46 +0200

    Add files via upload

---
+++ ROLE.sql
@@ -0,0 +1,35 @@
+CREATE ROLE professor WITH LOGIN;
+GRANT CREATE, CONNECT ON DATABASE Pokemon TO professor;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO professor;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO professor;
+GRANT ALL PRIVILEGES ON ALL PROCEDURES IN SCHEMA public TO professor;
+GRANT ALL PRIVILEGES ON SCHEMA public TO professor;
+
+
+CREATE ROLE breeder WITH LOGIN;
+GRANT SELECT, INSERT, UPDATE ON TABLE pokemon TO breeder;
+GRANT SELECT, INSERT, UPDATE ON TABLE species TO breeder;
+GRANT SELECT, INSERT, UPDATE ON TABLE pokemon_type TO breeder;
+GRANT SELECT, INSERT, UPDATE ON TABLE pokemon_ability TO breeder;
+GRANT SELECT, INSERT, UPDATE ON TABLE pokemon_stat TO breeder;
+GRANT EXECUTE ON FUNCTION type_change, update_pokemon_weight_and_height TO breeder;
+
+
+CREATE ROLE professors_assistant WITH LOGIN;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO professors_assistant;
+GRANT EXECUTE ON FUNCTION get_pokemon_stats, get_pokemon_by_generation, get_effectiveness, get_pokemon_abilities, get_pokemon_by_type TO professors_assistant;
+
+
+CREATE ROLE bug_catcher WITH LOGIN;
+GRANT SELECT ON TABLE pokemon TO bug_catcher;
+GRANT SELECT ON TABLE species TO bug_catcher;
+GRANT SELECT ON TABLE type TO bug_catcher;
+GRANT SELECT ON TABLE ability TO bug_catcher;
+GRANT SELECT ON TABLE game TO bug_catcher;
+GRANT SELECT ON TABLE pokedex TO bug_catcher;
+
+
+GRANT professor TO oak;
+GRANT breeder TO red;
+GRANT professors_assistant TO toni, alex;
+GRANT bug_catcher TO pingo;
